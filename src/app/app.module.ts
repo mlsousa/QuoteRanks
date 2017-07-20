2da5962c863ecebf86afdd8b31740c0b984a25c3
mlsousa <mike.sousa@yahoo.com>
2017-07-20 16:47:31 -0700

    initial commit - broken at quotecomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteListComponent } from './quote-list/quote-list.component';
+import { QuoteCreateComponent } from './quote/quote-create/quote-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteListComponent,
+    QuoteCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/quote.ts
@@ -0,0 +1,9 @@
+export class Quote {
+    constructor(
+    public id: number = null,
+    public quote: string = '',
+    public author: string = ''
+
+    ){}
+
+}
